e3cedefa50cb42c0a452955f7e72cb35b50ec039
unknown <hasanur23910@gmail.com>
2026-01-13 11:02:58 +0600

    cache

---
+++ src/subscribers/cache-handler.ts
@@ -0,0 +1,16 @@
+import type {
+  SubscriberArgs,
+  SubscriberConfig,
+} from "@medusajs/framework"
+
+export default async function productUpdatedHandler({
+  event: { data },
+  container,
+}: SubscriberArgs<{ id: string }>) {
+  // send request to next.js storefront to revalidate cache
+  await fetch(`${process.env.STOREFRONT_URL}/api/revalidate?tags=products`)
+}
+
+export const config: SubscriberConfig = {
+  event: "product.updated",
+}
